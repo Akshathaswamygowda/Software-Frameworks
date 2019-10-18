e042a633bbceb5272b4f69446d7c33f94f7224d9
Akshatha Thonnuru Swamygowda <42483354+Akshathaswamygowda@users.noreply.github.com>
2019-10-18 11:12:43 -0700

    Add files via upload

---
+++ CECS475Assignment 1/cecs575Assignment1/IntegerSet.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cecs575Assignment1
+{ 
+    //IntegerSet class for performing set operation
+    class IntegerSet
+    {
+        /// <summary>
+        /// Each Integer Set is represented by boolean array .</summary>
+        /// 
+        public static int size = 101;
+        private bool[] array=new bool[size] ;
+
+        /// <summary>
+        /// class  constructor. </summary>
+
+        public IntegerSet()                                                   
+        {
+            
+            array.DefaultIfEmpty<bool>();                                    
+
+        }
+        /// <summary>
+        /// Parameter Constructor
+        /// </summary>
+        /// <param name="intarray"></param>
+        public IntegerSet(int[] intarray)                                   
+        {
+            for (int i = 0; i < intarray.Length; i++)                     // Intializes the interger set to boolen array
+            {
+                if (intarray[i] >= 0 && intarray[i] <= 101)
+                {
+                    array[intarray[i]] = true;                           //Setting boolean array elements true based on integer set values 
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method for inserting num into set
+        /// </summary>
+        /// <param name="num"></param>
+        public void Insertset(int num)                                 
+        {
+            if (num > 0 && num <= 101)
+            {
+                this.array[num] = true;                                 //sets the boolean array element to true
+            }
+        }
+
+        /// <summary>
+        /// Method for deleting num from set
+        /// </summary>
+        /// <param name="num"></param>
+        public void Deleteset(int num)                                  
+        {
+            if (num > 0 && num <= 101)
+            {
+                this.array[num] = false;                                //sets the boolean array element to false
+            }
+        }
+
+        /// <summary>
+        /// Method for displaying the set elements
+        /// </summary>
+        /// <returns>returns string value</returns>
+        public string ToString()                                        
+        {
+            string display="";
+           
+            for(int i=0;i<array.Length;i++)
+            {
+               
+                if (array[i] == true)                                    //checks whichever array element is true  
+                {
+                    display = display + "  " + i.ToString();             //array elements with true value will added to the string   
+                }
+            }
+            if (display == "")                                           //Checks if the set is empty
+            {
+                Console.WriteLine("---");
+            }
+            return display;                                         //return string elements
+        }
+
+        /// <summary>
+        /// Method for comapring two sets to check if its equal
+        /// </summary>
+        /// <param name="ob"></param>
+        /// <returns>return bool value</returns>
+        public bool IsEqualTo(IntegerSet ob)                            
+        {
+            for(int i = 0; i < size; i++)
+            {
+                if (array[i] != ob.array[i])                             //two set objects
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// Method for union operation
+        /// </summary>
+        /// <param name="ob"></param>
+        /// <returns>returns class object</returns>
+        public IntegerSet Union(IntegerSet ob)                          
+        {
+            IntegerSet set4 = new IntegerSet();                         //creates new instance for displaying union of sets
+            bool[] union = new bool[size];
+            for(int i=0;i<size;i++)
+            {
+                
+                if(array[i] || ob.array[i])                            
+                {
+                    union[i] = true;
+                }
+            }
+            set4.array = union;
+            return set4;
+        }
+
+        /// <summary>
+        /// Method for intersection operation
+        /// </summary>
+        /// <param name="ob"></param>
+        /// <returns>returns class object value</returns>
+        public IntegerSet Intersection(IntegerSet ob)                   
+        {
+            IntegerSet set5 = new IntegerSet();                        //creates new instance for displaying intersection of sets
+            bool[] inter = new bool[size];
+            for(int i=0;i<size;i++)
+            {
+                if(array[i] && ob.array[i])                          
+                {
+                    inter[i] = true;
+                }
+            }
+            set5.array = inter;
+            return set5;
+        }
+    }
+}
---
+++ CECS475Assignment 1/cecs575Assignment1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cecs575Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Set A");
+            IntegerSet set1 = inputset();
+            Console.WriteLine("Input Set B");
+            IntegerSet set2 = inputset();
+            Console.WriteLine("\nSet A contains elements:");
+            Console.WriteLine(set1.ToString());
+            Console.WriteLine("\nSet B contains elements:");
+            Console.WriteLine(set2.ToString());
+            if (set1.IsEqualTo(set2))
+            {
+                Console.WriteLine("Set A is equal to Set B");
+            }
+            else
+                Console.WriteLine("Set A is not equal to B");
+
+            IntegerSet union = set1.Union(set2);
+            Console.WriteLine("Union of set A and set B contains");
+            Console.WriteLine(union.ToString());
+
+            IntegerSet intersection = set1.Intersection(set2);
+            Console.WriteLine("Intersection of set A and set B contains");
+            Console.WriteLine(intersection.ToString());
+            int[] intarray = { 25, 67, 2, 9, 99, 105, 45, -5, 100, 1 };
+            IntegerSet set3 = new IntegerSet(intarray);
+            Console.WriteLine("New set contains");
+            Console.WriteLine(set3.ToString());
+
+            Console.WriteLine("Insert Element 77");
+            set3.Insertset(77);
+            Console.WriteLine("New Set Contains");
+            Console.WriteLine(set3.ToString());
+
+            Console.WriteLine("Delete Element 77");
+            set3.Deleteset(77);
+            Console.WriteLine("New Set Contains");
+            Console.WriteLine(set3.ToString());
+            Console.ReadLine();
+
+        }
+        
+
+        /// <summary>
+        /// Method for taking input from user
+        /// </summary>
+        /// <returns>returns class object</returns>
+        private static IntegerSet inputset()
+        {
+
+            Char ch;
+            IntegerSet set1 =new IntegerSet(); //Instance of class
+            List<int> lis = new List<int>();   //for storing input values 
+
+             //for taking input
+            do                                           //Loop for continuing 
+            {
+                Console.WriteLine("Enter the number to Set");
+                int A = Convert.ToInt32(Console.ReadLine());
+                if (A > 0 && A < 101)                   //to check the input is withini valid range
+                {
+                    lis.Add(A);                        //Adding input to the list 
+                    set1 = new IntegerSet(lis.ToArray());//Assigning list elements to the class instance
+                    
+                }
+                //statement for asking to add another element into set
+                Console.WriteLine("Do you want to add element (Y/N)?");
+                ch = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            } while (ch.Equals(Char.Parse("Y")));   //condition for continuing the loop
+            return set1;
+        }
+    }
+}
